3908d4d5df95e8b6b580108d38672b8b4293c54b
hrarnob07 <hrarnob07@gmail.com>
2018-04-15 11:52:42 +0600

    first commit

---
+++ CRUD/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string name { get; set; }
+        public int Salary{ get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ CRUD/Models/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class EmployeeDbContext :DbContext
+    {
+        DbSet<Employee> Employees { get; set; }
+
+        public System.Data.Entity.DbSet<CRUD.Models.Employee> Employees { get; set; }
+    }
+}
